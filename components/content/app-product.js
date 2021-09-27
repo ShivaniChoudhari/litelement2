8ab286acc3a54bc14dbd1bdf5bbc005e116fcf9e
ShivaniChoudhari <91112250+ShivaniChoudhari@users.noreply.github.com>
2021-09-27 18:12:29 +0530

    New

--- components/content/app-product.js
+++ components/content/app-product.js
@@ -7,7 +7,19 @@ export default class AppProduct extends LitElement{
             font-weight: bold;
             font-size: large;
             text-align: center;
-        }`
+
+        
+        }
+        .container-fluid {
+            
+            display: flex;
+            flex-wrap: nowrap;
+            flex-direction : row;
+        }
+        
+        
+  
+        `
     }
     static get properties(){
         return{
@@ -19,18 +31,22 @@ export default class AppProduct extends LitElement{
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <script src='https://kit.fontawesome.com/a076d05399.js'></script>
         <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-2">  
-                <div class="card" style="width:100%; height:300px">
+        
+        <!-- <div class="row"> -->
+            <!-- <div class="col-md-2">   -->
+            <div class="card" style="width:100%; height:300px">
                     <a href="#"><img class="card-img-top" src="${this.product.image}" class="rounded-circle" alt="Card image" style="width:100%; height:200px"></a>
                     <div class="card-body">
                       <h4 class="card-title" style="text-align: center;">${this.product.header}</h4>
                       <!-- <p class="card-text"> For primary & secondary school kids, you can shop for larger backpacks with subtle designs in solid colors. Our collection is inclusive of school bags & backpack.</p> -->
                       ${this.product.offer ? html`<h5>${this.product.offer}</h5>`  : null}
                     </div>
-                  </div>
+                </div>
             </div>
-        </div>    `
+        </div>
+    </div>    
+        `
+        
     }
 }
 customElements.define('app-product', AppProduct);
